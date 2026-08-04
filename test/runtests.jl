385cc6794cdad1341f668cacca8f0904ed3e499d
MarkNahabedian <naha@mit.edu>
2026-08-04 09:22:50 -0400

    revise the modifier unit tests now that we have Processes.

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,19 +24,10 @@ end
 end
 
 @testset "recipie ingredient modifiers" begin
-    meths = cat(map(methods, [
-        smelt_ingredient_scalar,
-        craft_ingredient_scalar
-    ])..., dims=1)
-    specs = filter(t -> isconcretetype(t) && t <: Project,
-                   Set(map(meths) do m
-                           Base.unwrap_unionall(m.sig).parameters[2]
-                       end))
-    modifiers = map(s -> s(), collect(specs))
     r = rx"PlatinumBar"
     @test delta(r, []) ==
         Platinum(-1000.0) + GoldBar(-2.0) + PlatinumBar(1)
-    @test delta(r, modifiers) ==
+    @test delta(r, [SmeltingEfficiency()]) ==
         Platinum(-800.000) + GoldBar(-1.600) + PlatinumBar(1)
 end
 
@@ -48,25 +39,26 @@ end
 end
 
 @testset "project modifiers" begin
-    @test isapprox(smelt_ingredient_scalar(Management()), 1.0)
-    @test isapprox(craft_ingredient_scalar(Management()), 1.0)
-    @test isapprox(smelt_ingredient_scalar(Management()), 1.0)
-    @test isapprox(craft_ingredient_scalar(Management()), 1.0)
-    @test isapprox(smelt_ingredient_scalar(SmeltingEfficiency()), 0.8)
-    @test isapprox(craft_ingredient_scalar(CraftingEfficiency()), 0.8)
-    @test isapprox(smelt_ingredient_scalar(SmeltingEfficiency()), 0.8)
-    @test isapprox(craft_ingredient_scalar(CraftingEfficiency()), 0.8)
+    @test isapprox(process_ingredient_scalar(Smelt(), SmeltingEfficiency()), 0.8)
+    @test isapprox(process_ingredient_scalar(Craft(), SmeltingEfficiency()), 1.0)
+    @test isapprox(process_ingredient_scalar(Research(), SmeltingEfficiency()), 1.0)
+    @test isapprox(process_ingredient_scalar(Smelt(), CraftingEfficiency()), 1.0)
+    @test isapprox(process_ingredient_scalar(Craft(), CraftingEfficiency()), 0.8)
+    @test isapprox(process_ingredient_scalar(Research(), CraftingEfficiency()), 1.0)
+    @test isapprox(process_ingredient_scalar(Smelt(), Management()), 1.0)
+    @test isapprox(process_ingredient_scalar(Craft(), Management()), 1.0)
+    @test isapprox(process_ingredient_scalar(Research(), Management()), 1.0)
 end
 
 @testset "room modifiers" begin
-    @test isapprox(smelt_ingredient_scalar(Robotics(3)), 1)
-    @test isapprox(smelt_ingredient_scalar(Underforge(3)), 0.82)
-    @test isapprox(craft_ingredient_scalar(Engineering(2)), 1)
-    @test isapprox(craft_ingredient_scalar(Dorm(4)), 0.78)
-    @test isapprox(smelt_speed_scalar(Engineering(2)), 1)
-    @test isapprox(smelt_speed_scalar(Forge(3)), 1.4)
-    @test isapprox(craft_speed_scalar(Astronomy(2)), 1)
-    @test isapprox(craft_speed_scalar(Workshop(4)), 1.5)
+    @test isapprox(process_ingredient_scalar(Smelt(), Robotics(3)), 1)
+    @test isapprox(process_ingredient_scalar(Smelt(), Underforge(3)), 0.82)
+    @test isapprox(process_ingredient_scalar(Craft(), Engineering(2)), 1)
+    @test isapprox(process_ingredient_scalar(Craft(), Dorm(4)), 0.78)
+    @test isapprox(process_speed_scalar(Smelt(), Engineering(2)), 1)
+    @test isapprox(process_speed_scalar(Smelt(), Forge(3)), 1.4)
+    @test isapprox(process_speed_scalar(Craft(), Astronomy(2)), 1)
+    @test isapprox(process_speed_scalar(Craft(), Workshop(4)), 1.5)
 end
 
 @testset "add_researched_project" begin
@@ -74,5 +66,79 @@ end
     l1 = length(have)
     add_researched_project(Telescope5, have)
     @test length(have) == l1 + 4
+end
+
+@testset "process_ingredient_scalar spot check" begin
+    # This test was taken from an early set of game state calculations.
+    modifiers = Modifier[
+        # Rooms:
+        Engineering(8), Aeronautical(5), Packaging(5), Forge(7),
+        Workshop(7), Astronomy(2), Laboratory(3), Terrarium(4), Lounge(5),
+        BackupGenerator(2), Underforge(4)
+    ]
+    # Projects:
+    add_researched_project(SuperiorFurnace, modifiers)
+    add_researched_project(PreferredVendor, modifiers)
+    add_researched_project(SmeltingSpecialist, modifiers)
+    add_researched_project(AdvancedAlloyValue, modifiers)
+    add_researched_project(ColonizationEfficiency, modifiers)
+    add_researched_project(ColonyAdvancedTaxIncentives, modifiers)
+    add_researched_project(ColonizationSuperiorScouting, modifiers)
+    add_researched_project(AsteroidHarvester, modifiers)
+    add_researched_project(RoverScanningModule, modifiers)
+    add_researched_project(RoverResupply, modifiers)
+    add_researched_project(Telescope8, modifiers)
+    add_researched_project(OreTargeting, modifiers)
+    add_researched_project(BottleneckOptimizations, modifiers)
+    add_researched_project(Beacon, modifiers)
+    add_researched_project(SuperiorThrusters, modifiers)
+    add_researched_project(SuperiorCargoHandling, modifiers)
+    add_researched_project(CraftingSpecialist, modifiers)
+    add_researched_project(CraftingEfficiency, modifiers)
+    add_researched_project(AsteroidAutoMiner, modifiers) 
+    add_researched_project(PreferredVendor, modifiers)
+    add_researched_project(Terraforming, modifiers)
+    add_researched_project(InsideTrader, modifiers)
+    add_researched_project(SuperiorAlloyValue, modifiers)
+    add_researched_project(Telescope9, modifiers)
+    add_researched_project(ManagerTraining, modifiers)
+    add_researched_project(SuperiorCrafting, modifiers)
+    function rnd(i::Inventory)
+        Inventory([
+            typeof(item)(round(Int, item.count))
+            for item in i.items])
+    end
+    @test rnd(delta(rx"Alchemy", modifiers)) ==
+        GoldBar(41) + Lens(5)
+    @test rnd(delta(rx"ColonyRenegotiation", modifiers)) ==
+        BronzeBar(82) + Hammer(328)
+    @test rnd(delta(rx"ContractManager", modifiers)) ==
+        TitaniumBar(20) + Circuit(16) + BasicComputer(16)
+    @test rnd(delta(rx"Telescope10", modifiers)) ==
+        PalladiumBar(8) + ThermalScanner(2)
+    @test rnd(delta(rx"ColonySuperiorTaxIncentives", modifiers)) ==
+        Inventory(PalladiumBar(49))
+    @test rnd(delta(rx"AdvancedManagerTraining", modifiers)) ==
+        AdvancedBattery(8) + AdvancedComputer(2)
+    @test rnd(delta(rx"ColonizationSuperiorEfficiency", modifiers)) ==
+        PalladiumBar(41) + LaserTorch(12)
+    @test rnd(delta(rx"FurnaceOverdrive", modifiers)) ==
+        OsmiumBar(16) + PlasmaTorch(1)
+    @test rnd(delta(rx"AdvancedOreTargeting", modifiers)) ==
+        BasicComputer(82) + ThermalScanner(12)
+    @test rnd(delta(rx"MarketManipulation", modifiers)) ==
+        Diamond(24600) + GoldBar(12300) + BasicComputer(8)
+    @test rnd(delta(rx"SuperiorItemValue", modifiers)) ==
+        PalladiumBar(164) + LaserTorch(20)
+    @test rnd(delta(rx"AsteroidScanner", modifiers)) ==
+        ThermalScanner(25) + NavigationModule(4)
+    @test rnd(delta(rx"AdvancedRoverResupply", modifiers)) ==
+        RhodiumBar(20) + AdvancedBattery(8) + PlasmaTorch(5)
+    @test rnd(delta(rx"MarketAccelerator", modifiers)) ==
+        IridiumBar(328) + Motor(1)
+    @test rnd(delta(rx"AdvancedTerraforming", modifiers)) ==
+        Inventory(SatelliteDish(8))
+    @test rnd(delta(rx"AdvancedAsteroidHarvester", modifiers)) ==
+        PlasmaTorch(41) + SpaceProbe(1)
 end
 
