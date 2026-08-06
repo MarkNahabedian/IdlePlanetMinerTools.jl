0bf8f35a3c195d263c0adaa63caec5fd3b3ce2ef
MarkNahabedian <naha@mit.edu>
2026-08-06 17:00:17 -0400

    more orders of magnitude.

--- src/things.jl
+++ src/things.jl
@@ -108,7 +108,10 @@ PARSE_MATERIALS_MULTIPLIER_SUFFIXES = Dict([
     "T" => 10 ^ (3 * 4),
     "q" => 10 ^ (3 * 5),
     "Q" => 10 ^ (3 * 6),
-    "s" => 10.0 ^ (3 * 7)
+    "s" => 10.0 ^ (3 * 7),
+    "S" => 10.0 ^ (3 * 8),
+    "O" => 10.0 ^ (3 * 9),
+    "N" => 10.0 ^ (3 * 10)
 ])
 
 function parse_selling_price(s::AbstractString)
