3c4ed858969ca6fe7d93d61bae364e73dbf5d146
MarkNahabedian <naha@mit.edu>
2026-08-06 16:59:46 -0400

    Telescope24 was missing from the Projects wiki page.  Patch it in by hand.

--- src/extract_projects.jl
+++ src/extract_projects.jl
@@ -154,3 +154,12 @@ end
 
 make_project_definitions()
 
+# Telescope 24 is not on the wiki page:
+struct Telescope24 <: Project end
+@doc "Extends vision to planets 74 - 76" Telescope24
+prerequisites(::Telescope24) = [Telescope23]
+Recipie(Telescope24,
+        Teleporter(5) + AqualiteAlloy(21),
+        0)
+export Telescope24
+
