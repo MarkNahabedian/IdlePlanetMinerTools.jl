2b46766198db5700bb74953770e53812e5fbdee4
MarkNahabedian <naha@mit.edu>
2026-08-09 11:08:19 -0400

    NoAdsMineBoots

--- src/modifiers.jl
+++ src/modifiers.jl
@@ -1,6 +1,6 @@
 using Markdown
 
-export Modifier, DEFAULT_MODIFIERS, doctext
+export Modifier, NoAdsMineBoots, DEFAULT_MODIFIERS, doctext
 
 
 """
@@ -9,6 +9,15 @@ various arithmetic factors in the game, for example [`Project`](@ref)s
 or `Rooms` that modify smelting ingredients.
 """
 abstract type Modifier end
+
+
+"""
+NoAdsMineBoots is the modifier that provides a 1.2 increase in mining
+rate when you pay for no ads.
+"""
+struct NoAdsMineBoots <: Modifier
+end
+
 
 """
 DEFAULT_MODIFIERS is the list of Modifiers that is used for operations
